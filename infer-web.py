3af186cc774c90fffb83c7d1d5c1fb29de984117
Alex <56201083@qq.com>
2026-03-31 16:40:18 +0800

    修改端口号

--- infer-web.py
+++ infer-web.py
@@ -1614,6 +1614,6 @@ with gr.Blocks(title="RVC WebUI") as app:
         app.queue(concurrency_count=511, max_size=1022).launch(
             server_name="0.0.0.0",
             inbrowser=not config.noautoopen,
-            server_port=config.listen_port,
+            server_port=9999,
             quiet=True,
         )
